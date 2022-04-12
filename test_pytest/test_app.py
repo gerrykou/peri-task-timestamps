0d99859fdbf222db85d8de22342e4f2cc3dda2ba
gerrykou <gerasimoskounadis@gmail.com>
2022-04-12 16:20:52 +0100

    add pytest folder

--- test_pytest/test_app.py
+++ test_pytest/test_app.py
@@ -3,15 +3,15 @@ import pytest
 import src.app as app
 
 #01
-def test_datetime_obj2string(self):
+def test_datetime_obj2string():
     test_input = datetime.datetime(2021, 7, 14, 20, 46, 3)
     expected_output = '20210714T204603Z'
     actual_output = app._datetime_obj2string(test_input)
-    self.assertEqual(expected_output, actual_output)
+    assert expected_output == actual_output
 
 #02
-def test_validate_t2_greaterorequal_than_t1(self):
+def test_validate_t2_greaterorequal_than_t1():
     test_input_time1 ='20211115T123456Z'
     test_input_time2 = '20180214T204603Z'
-    with self.assertRaises(SystemExit):
+    with pytest.raises(SystemExit): 
         app._validate_t2_greaterorequal_than_t1(test_input_time1,test_input_time2)
